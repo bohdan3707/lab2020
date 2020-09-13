1b5f7946dfb9fd300901af8f59a407604652b4b4
Bohdan Yurchenko <bogdan3707@gmail.com>
2020-09-13 17:31:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab1sys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть до якого числа Фібоначі рахувати");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int perv = 1;
+            Console.Write("{0} ", perv);
+            int vtor = 1;
+            Console.Write("{0} ", vtor);
+            int sum = 0;
+
+            while (n >= sum)
+            {
+                sum = perv + vtor;
+
+                Console.Write("{0} ", sum);
+
+                perv = vtor;
+                vtor = sum;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
